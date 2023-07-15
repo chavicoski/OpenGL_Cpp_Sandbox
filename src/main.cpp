0343c67ca3fd727f49104d5ff15b549748ed9ace
chavicoski <alvaro.lopez.chilet@gmail.com>
2023-07-15 20:12:24 +0200

    First commit. Adding GLFW

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+  GLFWwindow *window;
+
+  if (!glfwInit()) {
+    std::cout << "GLFW could not be initialized" << std::endl;
+    return 1;
+  }
+
+  window = glfwCreateWindow(640, 480, "OpenGL Sandbox", NULL, NULL);
+
+  return 0;
+}
